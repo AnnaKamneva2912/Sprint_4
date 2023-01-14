ad9ce985d7abef100b0dd76d331a92d3912f3300
Anna Kamneva <anna.kamneva2912@gmail.com>
2023-01-14 09:27:53 +0300

    first commit

---
+++ locators/logo_samokat_locator.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+class LogoSamokatLocators:
+    upper_button_order = [By.XPATH, '//button[text()="Заказать"]']
+    logo_samokat = [By.XPATH, '//img[@alt="Scooter"]']
+    scooter_image = [By.XPATH, '//img[@alt="Scooter blueprint"]']
---
+++ tests/logo_samokat_tests.py
@@ -0,0 +1,12 @@
+
+from locators.logo_samokat_locator import LogoSamokatLocators
+from pages.logo_samokat_page import LogoSamokatPage
+
+
+class TestLogoSamokat():
+    def test_logo_samokat(self, driver):
+        driver.get('https://qa-scooter.praktikum-services.ru/')
+        samokat_logo = LogoSamokatPage(driver)
+        samokat_logo.click_logo_samokat()
+        element_scooter = driver.find_element(*LogoSamokatLocators.scooter_image)
+        assert element_scooter.is_displayed()
