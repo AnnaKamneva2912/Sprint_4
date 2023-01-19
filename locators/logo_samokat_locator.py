6b1c779d30877981dcf67cd48cca7e9c9f1fe25c
Anna Kamneva <anna.kamneva2912@gmail.com>
2023-01-19 18:38:24 +0300

    third commit

--- locators/logo_samokat_locator.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class LogoSamokatLocators:
-    upper_button_order = [By.XPATH, '//button[text()="Заказать"]']
-    logo_samokat = [By.XPATH, '//img[@alt="Scooter"]']
-    scooter_image = [By.XPATH, '//img[@alt="Scooter blueprint"]']
--- locators/logo_yandex_locator.py
+++
@@ -1,4 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class LogoYandexLocators:
-    logo_yandex = [By.XPATH, '//img[@alt="Yandex"]']
